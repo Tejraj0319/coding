c762e814c96210752a3bdc54e5ed9e54c56eb41b
Rajesh Ananta Patil <139704615+Tejraj0319@users.noreply.github.com>
2023-07-28 22:13:08 +0530

    Add files via upload

---
+++ Day_17.java
@@ -0,0 +1,20 @@
+
+package Simple;
+
+import java.util.Scanner;
+
+public class Day_17
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in); 
+        System.out.println("Enter the number: ");
+        int num=sc.nextInt();
+
+        for(int i=1;i<=num;i++)
+            if(num%i==0)
+            {
+                System.out.print(i+" ");
+            }
+    }
+}
